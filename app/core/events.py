11e37a84524038a966a64feae222c44d618e3d09
parinaysethtecholution <parinay.seth@techolution.com>
2025-05-01 16:04:36 +0530

    chore: Comment out Google Cloud authentication code in lifespan function

--- app/core/events.py
+++ app/core/events.py
@@ -12,18 +12,18 @@ import app.core.config as config
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     
+    logging.info("Starting application")
+    # command = ["gcloud", "auth", "activate-service-account", f"--key-file={config.GOOGLE_APPLICATION_CREDENTIALS}"]
+    # process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    # stdout, stderr = process.communicate()
+    
+    # if process.returncode != 0:
+    #     logging.error(f"Google Cloud Authentication Error: {stderr.decode()}")
+    #     raise RuntimeError("Failed to authenticate with Google Cloud")
+    # else:
+    #     logging.info("Successfully authenticated with Google Cloud")
 
-    command = ["gcloud", "auth", "activate-service-account", f"--key-file={config.GOOGLE_APPLICATION_CREDENTIALS}"]
-    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stdout, stderr = process.communicate()
-    
-    if process.returncode != 0:
-        logging.error(f"Google Cloud Authentication Error: {stderr.decode()}")
-        raise RuntimeError("Failed to authenticate with Google Cloud")
-    else:
-        logging.info("Successfully authenticated with Google Cloud")
-
-    yield
+    # yield
     
     
-    logging.info("Application is shutting down")
+    # logging.info("Application is shutting down")
